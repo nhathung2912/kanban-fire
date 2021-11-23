5862941b0c78f274e657d0923b80ae7baac24db4
nhathung2912 <hungc4k43@gmail.com>
2021-11-23 21:45:40 +0100

    fiest commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBbsOEmSeAC3COLmD_XNOIXfWXt2BzmGP0",
+    authDomain: "kanbanfire-85da2.firebaseapp.com",
+    databaseURL: "https://kanbanfire-85da2-default-rtdb.firebaseio.com",
+    projectId: "kanbanfire-85da2",
+    storageBucket: "kanbanfire-85da2.appspot.com",
+    messagingSenderId: "242719282976",
+    appId: "1:242719282976:web:085ee4faedab168a486945",
+    measurementId: "G-SES39JGQVQ"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,21 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { initializeApp } from "@firebase/app";
+import { getAnalytics } from 'firebase/analytics';
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBbsOEmSeAC3COLmD_XNOIXfWXt2BzmGP0",
+    authDomain: "kanbanfire-85da2.firebaseapp.com",
+    databaseURL: "https://kanbanfire-85da2-default-rtdb.firebaseio.com",
+    projectId: "kanbanfire-85da2",
+    storageBucket: "kanbanfire-85da2.appspot.com",
+    messagingSenderId: "242719282976",
+    appId: "1:242719282976:web:085ee4faedab168a486945",
+    measurementId: "G-SES39JGQVQ"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
